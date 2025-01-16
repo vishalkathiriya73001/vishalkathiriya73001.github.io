d654e42e1532ff397b2e1a601c3751056ba28875
pethanihiten <pethanihiten@gmail.com>
2025-01-16 18:40:55 +0530

    100 TOP

--- Saver/followers/apicall.php
+++ Saver/followers/apicall.php
@@ -3,45 +3,27 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Path to your JSON file
 $json_file = '100.json';
-
-// https://socialbook.io/instagram-channel-rank/top-100-instagrammers
-// https://socialbook.io/api/tool/channel/rank_list?size=100&channel=instagram&country=all&category=all&sort_field=followers
-
-// Read the JSON file
 $json_data = file_get_contents($json_file);
-
-// Decode JSON data
 $data = json_decode($json_data, true);
-
 if ($data === null) {
     die('Failed to decode JSON data');
 }
-
 // print_r($data);
 
-// Accessing specific values
-// echo "Instagram username: " . $data['core_user']['instagram_username'] . "\n";
-// echo "Country: " . $data['country'] . "\n";
-// echo "Main topic: " . $data['main_topic'] . "\n";
-
-// Accessing brand mentions
 foreach ($data['data'] as $entry) {
-    // Access the value of 'is_agent' for the current entry
     $avatar = $entry['avatar'];
     $filename = pathinfo($avatar, PATHINFO_FILENAME);
     $url = "https://socialbook.io/api/core_user/trend?user_stat_user_id=$filename&date_range=now-2y&channel=instagram";
+
     $json_data = file_get_contents($url);
     $filename = $filename . '.json';
-
     if ($json_data === false) {
         die('Failed to fetch JSON data from URL');
     }
-
     $save_path = __DIR__ . '/' . $filename;
-
-    // Save the JSON data to a file locally
+    print_r($save_path);
+    print_r("\n\n");
     if (file_put_contents($save_path, $json_data) !== false) {
         echo "File saved successfully: $save_path";
     } else {
@@ -49,4 +31,4 @@ foreach ($data['data'] as $entry) {
     }
 }
 
-
+?>
